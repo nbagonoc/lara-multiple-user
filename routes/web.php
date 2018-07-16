48938bc6a89eac368d1967329e35522c87c427dc
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-07-16 22:33:05 +0800

    fixed the /home redirect instead of dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+// home
 Route::get('/', [
     'uses' => 'PagesController@index',
     'as' => 'pages.home'
 ]);
 
+// dashboard
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+// admin
 Route::get('/admin', [
     'uses' => 'PagesController@admin',
     'as' => 'pages.admin'
 ])->middleware('admin');
-
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
 Auth::routes();
